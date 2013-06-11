9d822e6371fd0e5f3129f78733884f4f4363aed3
jackhall <jackwhall7@gmail.com>
2013-06-11 13:25:24 -0500

    rewrote field.py for modularity

--- field.py
+++ field.py
@@ -1,42 +1,66 @@
 import math
 import numpy
 
-class ProximityField(object):
-	def __init__(self, agents, width=1.0, height=1.0):
-		self.agents = agents #dict mapping IDs to agents
-		#Build self.points
-		self.update()
-		self.width = width
+
+def euclidean_distance(a, b):
+	return math.sqrt( sum(x-y for x, y in zip(a, b)) ) 
+
+
+class InverseSquare(object):
+	def __init__(self, height=1.0):
 		self.height = height
 
+	def __call__(self, a, b):
+		return self.height / (euclidean_distance(a, b)**2)
+
+
+class MexicanHat(object):
+	""" mexican hat functor """
+	def __init__(self, sigma=1.0):
+		self.sigma = sigma
+	
+	def __call__(self, a, b):
+		distance = euclidean_distance(a, b)
+		d_norm = (distance / self.sigma)**2
+		return ( (1 - d_norm) * math.exp(-0.5*d_norm) *
+				2/(math.sqrt(3.0*self.sigma) * math.pi**0.25) )
+
+
+class Field(object):
+	def __init__(self, agents, proximity=InverseSquare()):
+		self.agents = agents
+		self.proximity = proximity
+		self.update()
+
+	def field_value(self, point, pointID=None):
+		""" returns total field intensity following an inverse square
+			law in distance """
+		result = 0
+		for ID, location in self.points.iteritems():
+			if ID is not pointID:
+				result += self.proximity(point, location)
+		return result
+
+	def update(self):
+		""" updates internal storage of point data """
+		for ID, agent in self.agents.iter_items():
+			self.points[key] = agent.location 
+
+
+class VectorField(Field):
 	def field_value(self, point, pointID=None):
 		""" Returns the vector (numpy array) corresponding to the
 			given point. If a pointID is provided, that agent is
-			ignored. """
+			ignored. Expects agent locations to also be numpy arrays."""
 		p = numpy.array(point)
 		result = numpy.zeros(len(p))
-		for ID, location in self.points.iter_items():
+		for ID, location in self.points.iteritems():
 			#Compute the euclidian distance
-			distance = sqrt( sum([x-y for x, y in zip(p, location)]) ) 
-			if distance < 5.0*self.width and ID is not pointID:
-				magnitude = self.height * self._proximity_fcn(distance)
+			magnitude = self.proximity(p, location)
+			if ID is not pointID:
 				#Vector difference should point away from each location.
-				result += magnitude * (point - location)
+				#difference depends on elementwise operations like numpy.array
+				result += magnitude * (p - location) 
 		return result
-
-	def _proximity_fcn(self, d):
-		"""returns a simple distance scaled by self.width"""
-		return d / self.width
-
-	def update(self):
-		for ID, agent in self.agents.iter_items():
-			self.points[key] = agent.location
-
-
-class MexicanHatField(ProximityField):
-	def _proximity_fcn(self, d):
-		"""The mexican hat function with sigma = self._width"""
-		d_norm = (d / self.width)**2
-		return ( (1 - d_norm) * math.exp(-0.5*d_norm) *
-				2/(math.sqrt(3.0*self.width) * math.pi**0.25) )
+	
 
