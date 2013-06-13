8366da2b0fe87ab8ec75e7e4a0945f0cf7caacfb
jackhall <jackwhall7@gmail.com>
2013-06-13 12:46:47 -0500

    added G PL headers, split learning into separate file

--- agent.py
+++ agent.py
@@ -1,3 +1,21 @@
+#GraphSelfOrganization
+#Copyright (C) 2013  Matthew Ira Campbell, John Wendell Hall
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#The author may be reached at jackhall@utexas.edu.
+
 import yaml
 #import multiprocessing #will be used to split threads, avoiding the python GIL
 import random 
@@ -6,13 +24,6 @@ import benpy
 
 import field
 
-#is there existing python code for this? boost.python may be necessary
-class BaysianLearner(object):
-	def __init__(self, options):
-		self.options = options
-		#start from a uniform distribution? or randomize and distribute?
-		self.strategy = dict.fromkeys(options.iterkeys(), 1.0/len(options))
-	
 
 class Agent(object):
 	#actions = #define class for this? No, use graphsynth if possible.
---
+++ learner.py
@@ -0,0 +1,26 @@
+#GraphSelfOrganization
+#Copyright (C) 2013  Matthew Ira Campbell, John Wendell Hall
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#The author may be reached at jackhall@utexas.edu.
+
+#is there existing python code for this? boost.python may be necessary
+class BayesianLearner(object):
+	def __init__(self, options):
+		self.options = options
+		#start from a uniform distribution? or randomize and distribute?
+		self.strategy = dict.fromkeys(options.iterkeys(), 1.0/len(options))
+
+
