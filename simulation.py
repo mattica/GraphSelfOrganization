e5c00e617d112e84a4ca8805789915b43c770d42
jackhall <jackwhall7@gmail.com>
2013-08-15 17:51:39 -0500

    simulation.py heavily commented

--- simulation.py
+++ simulation.py
@@ -12,12 +12,19 @@ import agent as agt
 #relative imports to reflect subfolders
 
 class BoundedUniform(object):
+	"""
+	A probability distribution uniform over the given bounds. 
+	"""
 	def __init__(self, bounds):
-		#upper and lower may be sequences
-		#if they are, this generator provides a sequence of values
+		"""
+		Takes a sequence of length-2 tuples.
+		len(bounds) is the number of dimensions for the distribution.
+		Each length-2 tuple is (lower_bound, upper_bound)
+		"""
 		self.bounds = bounds
 
 	def __call__(self):
+		""" Return a single sample from the pdf. """
 		result = numpy.zeros(len(self.bounds))
 		for i, interval in enumerate(self.bounds):
 			result[i] = random.uniform(*interval) 
@@ -40,73 +47,90 @@ class BoundedUniform(object):
 
 class Simulation(object):
 	def __init__(self, num_agents=10, max_iterations=10, connectivity=0.2):
-		self.location_generator = BoundedUniform([(-4, 4), (-4, 4)])
-		self.connectivity = (connectivity if connectivity < 1 
-										 else 1.0/connectivity)
-		self.agents = {}
-		self.graph = networkx.Graph()
-		self.field = fld.VectorField(self.agents, fld.MexicanHatGradient(3))
-		for i in range(num_agents):
-			#spawn location is randomized according to location_generator
-			self.spawn_agent()
+		""" Sets up a simulation. """
+		self.agents = {} #empty dict to be populated later
 		self.max_iterations = max_iterations
-		#plotting bounds properly belongs in environment object, refactor!
-		#self.environment = None #for now (when? if simulation needs dynamic state outside of the agents, add examples)
-		#add a field to environment
-		#self.ruleset = #or some graphsynth object to encapusulate more details
-		#ruleset return a single list of possible options, agents will have to choose from ALL
 
-	def spawn_agent(self, location=None):
-		if location is None:
-			new_location = self.location_generator()
-		else:
-			new_location = location
-		new_agent = agt.Agent(new_location, self.field.field_value)
+		#Connectivity is the probability that any given link exists
+		#between agents. The next line ensures that the probability is not 
+		#greater than one or less than zero.
+		self.connectivity = abs(connectivity if abs(connectivity) < 1 
+										 else 1.0/connectivity)
+
+		#The default networkx graph is simple and undirected, empty by default. 
+		self.graph = networkx.Graph()
+
+		#The environment encapuslates any behavior that does not concern 
+		#the agents directly.
+		self.environment = None #add walls and a penalty field (with bounds)
+
+		#location_generator is a functor that samples from a probability 
+		#distribution; it's used to initialize agent locations.
+		self.location_generator = BoundedUniform([(-4, 4), (-4, 4)]) 
+
+		#A field is a function with a unique value for each position. This 
+		#field depends on the positions of agents.
+		self.field = fld.VectorField(self.agents, fld.MexicanHatGradient(3))
+
+		#This field depends on the environment.
+		#MAKE A NEW FIELD HERE
+
+		self.ruleset = [] 	#Rule objects should have a recognize method
+							#that returns a list of options. Agents will
+							#choose from ALL options.
+
+		#Populate the dict of agents...
+		#agents = {spawn_agent() for i in range(num_agents)} #using a set?
+		for i in range(num_agents):
+			self.spawn_agent()
+
+	def spawn_agent(self, location=self.location_generator()):
+		new_agent = agt.Agent(location, self.field.field_value)
 		self.graph.add_node(new_agent.ID, agent=new_agent)
 		for ID, agent in self.agents.iteritems():
+			#Create an link with probability = connectivity.
 			if random.random() < self.connectivity:
-				d = numpy.linalg.norm(new_agent.location - agent.location)
+				#d = numpy.linalg.norm(new_agent.location - agent.location)
 				self.graph.add_edge(new_agent.ID, ID)
 		self.agents[new_agent.ID] = new_agent
 		return new_agent
 
 	def run(self):
 		plt.ion()
-		#print "running..."
-		#fig = plt.figure()
-		#ax = fig.add_subplot()
-		start = time.clock()
-		k = 0
+		start = time.clock() #for timing the algorithm
+		iteration = 0
 		positions = {ID: agent.location.tolist() 
 					 for ID, agent in self.agents.iteritems()}
 		#no rules yet, but add them as a skeleton
 		options = [agt.Spread(), 
 				   agt.NormalizeLinks(self.graph, self.agents, step=0.2)]
-		#for i in self.converged(): #Using a generator!
-		while not self.converged(k):
-			#options = graphsynth.recognize(self.graph, rules) #the big board
-			#options.build_agent_assignments()
-			self.field.update()
-			#roll a recorder object
+
+		#main simulation loop
+		while not self.converged(iteration):
+			#plot state
 			plt.hold(False)
-			networkx.draw(self.graph, pos=positions) #only time positions gets used
-			plt.xlim((-10,10))
+			networkx.draw(self.graph, pos=positions) #sole use of positions
+			plt.xlim((-10,10)) #these should come from environment
 			plt.ylim((-10,10))
 			plt.draw()
-			#plt.show()
+
+			#update [proximity] field
+			self.field.update()
+
+			#generate list (one element/rule) of lists of options
+			options = [rule.recognize() for rule in self.ruleset]
+
+			#plt.show() #needed only if matplotlib interactive mode is off
 			agent_queue = self.agents.items() #actually need the list here
 			random.shuffle(agent_queue)
-			#options = rules.recognize(self.manager.graph)
 			for ID, agent in agent_queue:
-				#agent.choose(options[agent.ID])
-				#add options callback to remove already-used options
 				choice = agent.choose(options)
-				agent.act(choice)
-				positions[ID] = agent.location.tolist() #updates
-			#self.environment.step()
-			k += 1
+				choice.apply(agent)
+				positions[ID] = agent.location.tolist() #updates plot positions
+			#self.environment.step() #need if environment has state
+			iteration += 1
 		print "elapsed:", time.clock() - start
 
-	def converged(self, k):
-		return k > self.max_iterations
+	def converged(self, current_iteration):
+		return current_iteration > self.max_iterations
 
