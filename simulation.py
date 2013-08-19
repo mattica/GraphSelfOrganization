7b35e01d2dd69824e87035f46cad03f6dcd31a89
jackhall <jackwhall7@gmail.com>
2013-08-19 14:55:23 -0500

    README and rule docs left

--- simulation.py
+++ simulation.py
@@ -15,18 +15,17 @@ class BoundedUniform(object):
 	"""
 	def __init__(self, bounds):
 		"""
-		Takes a sequence of length-2 tuples.
-		len(bounds) is the number of dimensions for the distribution.
-		Each length-2 tuple is (lower_bound, upper_bound)
+		Takes a numpy array with two columns and n rows.
+		n is the number of dimensions for the distribution.
+		Each row is [lower_bound, upper_bound].
 		"""
 		self.bounds = bounds
 
 	def __call__(self):
 		""" Return a single sample from the pdf. """
-		result = numpy.zeros(len(self.bounds))
-		for i, interval in enumerate(self.bounds):
-			result[i] = random.uniform(*interval) 
-		return result
+		result = numpy.random.random(self.bounds.shape[0]) #rand [0,1)
+		result *= self.bounds[:,1] - self.bounds[:,0] #scale
+		return result + self.bounds[:,0] #shift
 
 	@property
 	def bounds(self):
@@ -34,39 +33,76 @@ class BoundedUniform(object):
 	
 	@bounds.setter
 	def bounds(self, new_bounds):
+		""" Basic format checking. """
 		try:
-			for a, b in new_bounds:
-				assert a < b
+			assert not any(new_bounds[:,0] >= new_bounds[:,1])
+			assert new_bounds.shape[-1] == 2
 		except:
-			raise ValueError("Bounds should be a sequence of pairs (a, b)," 
-								+ "where a < b.")
+			raise ValueError("Bounds should be a numpy array where the "
+					+ "innermost dimension is length-2, and pairs over "
+					+ "the outer dimensions follow a < b for [a, b].")
 		self._bounds = new_bounds
 
 
+class Environment(object):
+	""" 
+	This example environment object puts up walls and enforces them
+	with a penalty function. The penalty function is meant to be applied
+	without giving the agent a choice (it's not a rule!).
+	"""
+	def __init__(self, bounds, function=None):
+		""" 
+		Initialize with a set of cartesian bounds (same format as 
+		BoundedUniform) and a penalty function. The penalty function
+		should be compatible with element-wise operations.
+		"""
+		self.bounds = numpy.array(bounds)
+		self.function = function or (lambda a, b: 0.2/((b-a)**2))
+
+	def field_value(self, point):
+		""" Push away from the walls according to an inverse square law. """
+		b = numpy.array(self.bounds)
+		return self.function(b[:,0], point) + self.function(point, b[:,1])
+
+	def update(self):
+		#no internal states to track, so there's nothing to do here for now
+		pass
+
+
 class Simulation(object):
+	"""
+	The simulation object encapsulates all parameters of a simulation
+	for easy offline manipulation. This one is a demonstration of different
+	elements of CSO simulation. It does not solve any engineering problem.
+	It should draw a graph animation with the nodes bouncing around randomly.
+	"""
 	def __init__(self, num_agents=10, max_iterations=10, connectivity=0.2):
 		""" Sets up a simulation. """
 		self.agents = {} #empty dict to be populated later
 		self.max_iterations = max_iterations
+		
+		#The environment encapuslates any behavior that does not concern 
+		#the agents directly. This one adds walls with a penalty field.
+		#Agents are involuntarily push away from the walls.
+		self.environment = Environment([(-6, 6), (-6, 6)]) 
 
 		#Connectivity is the probability that any given link exists
 		#between agents. The next line ensures that the probability is not 
 		#greater than one or less than zero.
 		self.connectivity = abs(connectivity if abs(connectivity) < 1 
-										 else 1.0/connectivity)
+											 else 1.0/connectivity)
 
 		#The default networkx graph is simple and undirected, empty by default. 
 		#Agent IDs serve as the nodes, and each agent object is associated with
 		#its node in the graph. See the networkx docs for details.
 		self.graph = networkx.Graph()
 
-		#The environment encapuslates any behavior that does not concern 
-		#the agents directly.
-		self.environment = None #add walls and a penalty field (with bounds)
-
+		
 		#location_generator is a functor that samples from a probability 
 		#distribution; it's used to initialize agent locations.
-		self.location_generator = BoundedUniform([(-4, 4), (-4, 4)]) 
+		location_bounds = (self.environment.bounds + 
+						   numpy.array([[1, -1], [1, -1]])) #margins
+		self.location_generator = BoundedUniform(location_bounds) 
 
 		#A field is a function with a unique value for each position. This 
 		#field depends on the positions of agents.
@@ -121,8 +157,8 @@ class Simulation(object):
 			#Plot the state of the simulation (agents + environment).
 			plt.hold(False)
 			networkx.draw(self.graph, pos=positions) #sole use of 'positions'
-			plt.xlim((-10,10)) #these should come from environment
-			plt.ylim((-10,10))
+			plt.xlim(self.environment.bounds[0,:]) 
+			plt.ylim(self.environment.bounds[1,:])
 			plt.draw()
 
 			#Update [proximity] field.
@@ -137,13 +173,20 @@ class Simulation(object):
 			agent_queue = self.agents.items() #actually need the list here
 			random.shuffle(agent_queue)
 			for ID, agent in agent_queue:
+				agent.location += self.environment.field_value(agent.location)
 				choice = agent.choose(options)
 				choice.apply(agent)
 				positions[ID] = agent.location.tolist() #updates plot positions
-			#self.environment.step() #need if environment has state
+			self.environment.update() #need if environment has state
 			iteration += 1
 		print "elapsed:", time.clock() - start
 
 	def converged(self, current_iteration):
 		return current_iteration > self.max_iterations
+
+
+def run():
+	""" Show an example simulation. """
+	sim = simulation.Simulation(num_agents=20, max_iterations=40, connectivity=.3)
+	sim.run()
 
--- test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import simulation
-
-#Instantiate and run a simulation. 
-sim = simulation.Simulation(num_agents=20, max_iterations=40, connectivity=.3)
-sim.run()
-
